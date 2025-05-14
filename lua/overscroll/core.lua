2a1c21be9c482b85057ebc38bf5dc3b257f25421
Johan Brandby <j@brandby.se>
2025-05-14 21:16:15 +0200

    feat: add align-center function

--- lua/overscroll/core.lua
+++ lua/overscroll/core.lua
@@ -15,4 +15,16 @@ function M.draw()
   })
 end
 
+function M.align_center()
+  local window_height = vim.api.nvim_win_get_height(0)
+  local delta = math.floor(window_height / 2) + 1 - vim.fn.winline()
+  if delta ~= 0 then
+    local count = math.abs(delta)
+    local key = delta > 0 and "<C-y>" or "<C-e>"
+    local code = vim.api.nvim_replace_termcodes(key, true, false, true)
+    local command = string.format("%d%s", count, code)
+    vim.cmd("normal! " .. command)
+  end
+end
+
 return M
