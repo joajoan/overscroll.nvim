6d7703028aa97d5917fe2f2aaf133f0da63e47dd
Johan Brandby <j@brandby.se>
2025-05-15 22:04:53 +0200

    refactor: extract scroll logic to separate function

--- lua/overscroll/core.lua
+++ lua/overscroll/core.lua
@@ -1,5 +1,15 @@
 local ns_id = vim.api.nvim_create_namespace("overscroll")
 
+local function scroll(delta)
+  if delta ~= 0 then
+    local count = math.abs(delta)
+    local key = delta > 0 and "<C-y>" or "<C-e>"
+    local code = vim.api.nvim_replace_termcodes(key, true, false, true)
+    local command = string.format("%d%s", count, code)
+    vim.cmd("normal! " .. command)
+  end
+end
+
 local M = {}
 
 function M.draw()
@@ -18,13 +28,7 @@ end
 function M.align_center()
   local window_height = vim.api.nvim_win_get_height(0)
   local delta = math.floor(window_height / 2) + 1 - vim.fn.winline()
-  if delta ~= 0 then
-    local count = math.abs(delta)
-    local key = delta > 0 and "<C-y>" or "<C-e>"
-    local code = vim.api.nvim_replace_termcodes(key, true, false, true)
-    local command = string.format("%d%s", count, code)
-    vim.cmd("normal! " .. command)
-  end
+  scroll(delta)
 end
 
 return M
